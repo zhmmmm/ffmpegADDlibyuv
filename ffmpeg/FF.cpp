6402789bf93a4177ef101f94fdd5da357f4dd8b3
15982683979 <15982683979@163.com>
2020-01-14 19:31:06 +0800

    添加项目文件。

---
+++ ffmpeg/FF.cpp
@@ -0,0 +1,1 @@
+#include "FF.h"
---
+++ ffmpeg/libyuv/libyuv.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2011 The LibYuv Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef INCLUDE_LIBYUV_H_
+#define INCLUDE_LIBYUV_H_
+
+#include "include/libyuv/basic_types.h"
+#include "include/libyuv/compare.h"
+#include "include/libyuv/convert.h"
+#include "include/libyuv/convert_argb.h"
+#include "include/libyuv/convert_from.h"
+#include "include/libyuv/convert_from_argb.h"
+#include "include/libyuv/cpu_id.h"
+#include "include/libyuv/mjpeg_decoder.h"
+#include "include/libyuv/planar_functions.h"
+#include "include/libyuv/rotate.h"
+#include "include/libyuv/rotate_argb.h"
+#include "include/libyuv/row.h"
+#include "include/libyuv/scale.h"
+#include "include/libyuv/scale_argb.h"
+#include "include/libyuv/scale_row.h"
+#include "include/libyuv/version.h"
+#include "include/libyuv/video_common.h"
+
+#endif  // INCLUDE_LIBYUV_H_
